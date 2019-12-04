4f50228afd6472a2cb52a750e664689c18979152
Kyle Laster <kyle.laster@gmail.com>
2019-12-03 21:51:29 -0600

    initial

---
+++ OdeToFood.data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name="Kyle's Pizza", Location="Arkansas", Cuisine=CuisineType.Mexican},
+                new Restaurant { Id = 2, Name="Burer", Location="Texas", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 3, Name="French Fries", Location="Canada", Cuisine=CuisineType.Indian},
+            };
+        }
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
